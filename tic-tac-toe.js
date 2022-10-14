499b3df073e1fdf14b07926ee9cbe962fc79876d
annia <bowesna278@gmail.com>
2022-10-13 19:52:47 -0500

    Added finishing square functionality and cleaned up code

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,7 @@
 window.onload = function() {  
     let game_board = document.getElementById("board");
-    // console.log(game_board);
 
     let squares = game_board.children;
-    // console.log(squares);
 
     // creates board layout
     for(i = 0; i <= squares.length - 1; i++) {
@@ -11,16 +9,9 @@ window.onload = function() {
         let squareid = "sq" + i;
         squares[i].setAttribute("id", squareid); // adds ids to squares
     }
-    // console.log(squares);
     
-    var option1 = "X";
-    var option2 = "O";
-    var turn = 0;
-    var lastplayed;
-
     squares = document.querySelectorAll(".square");
     // squares = document.getElementsByClassName("square");
-    // console.log(squares);
 
     // adds and removes hover class to squares when hovering and not hovering
     squares.forEach(square => {
@@ -33,35 +24,36 @@ window.onload = function() {
         }
     });
 
-    var buttons = document.getElementsByClassName("btn");
+    var option1 = "X";
+    var option2 = "O";
+    var turn = 0;
+    var lastplayed;
 
-    for(i = 0; i <= buttons.length - 1; i++) {
-        buttons[i].addEventListener("click", function() {
-            window.location.reload();
-        });
-    }
-    
-    var track = []; // X O X O X O X O X
+    var track = []; // always X O X O X O X O X
     
     let msgbox = document.getElementById("status");
     let msgX = "Congratulations! X is the Winner!";
     let msgO = "Congratulations! O is the Winner!";
                 
-    // console.log(track);
+    var buttons = document.getElementsByClassName("btn");
+
+    for(i = 0; i <= buttons.length - 1; i++) {
+        buttons[i].addEventListener("click", function() {
+            window.location.reload();                 
+        });
+    }
 
     // adds an X or O to a square when clicked
     squares.forEach(square => {
-        square.onclick = function() {
-            // console.log("Clicked");
-            
+        square.onclick = function() {                        
             if(turn === 0) { 
                 square.innerHTML = option1;
                 lastplayed = option1;
                 square.classList.add("X");
                 track.push(option1);
 
-                // disallow value from changing when once clicked
-                square.onclick = false;
+                // disallows value from changing when once clicked
+                square.onclick = false; 
             }
 
             else {
@@ -85,13 +77,9 @@ window.onload = function() {
             }
 
             turn += 1;
-            // console.log(turn);
-            // console.log(track);
 
             if(track.length > 2) {
                 let square1 = document.getElementById("sq0").textContent;
-                //console.log(square1);
-
                 let square2 = document.getElementById("sq1").textContent;
                 let square3 = document.getElementById("sq2").textContent;
                 let square4 = document.getElementById("sq3").textContent;
@@ -102,34 +90,58 @@ window.onload = function() {
                 let square9 = document.getElementById("sq8").textContent;
 
                 if(square1 === "X" && square2 === "X" && square3 === "X") {
-                    // console.log(square1);
                     msgbox.textContent = msgX;
                     msgbox.classList.add("you-won");
+                    
+                    // disallows value from changing after a player has won
+                    squares.forEach(square => {
+                        square.onclick = false;
+                    });
                 }
 
                 if(square4 === "X" && square5 === "X" && square6 === "X") {
                     msgbox.textContent = msgX;
                     msgbox.classList.add("you-won");
+
+                    squares.forEach(square => {
+                        square.onclick = false;
+                    });
                 }
 
                 if(square7 === "X" && square8 === "X" && square9 === "X") {
                     msgbox.textContent = msgX;
                     msgbox.classList.add("you-won");
+
+                    squares.forEach(square => {
+                        square.onclick = false;
+                    });
                 }
 
                 if(square1 === "O" && square2 === "O" && square3 === "O") {
                     msgbox.textContent = msgO;
                     msgbox.classList.add("you-won");
+
+                    squares.forEach(square => {
+                        square.onclick = false;
+                    });
                 }
 
                 if(square4 === "O" && square5 === "O" && square6 === "O") {
                     msgbox.textContent = msgO;
                     msgbox.classList.add("you-won");
+
+                    squares.forEach(square => {
+                        square.onclick = false;
+                    });
                 }
 
                 if(square7 === "O" && square8 === "O" && square9 === "O") {
                     msgbox.textContent = msgO;
                     msgbox.classList.add("you-won");
+
+                    squares.forEach(square => {
+                        square.onclick = false;
+                    });
                 }
             }
         }        
